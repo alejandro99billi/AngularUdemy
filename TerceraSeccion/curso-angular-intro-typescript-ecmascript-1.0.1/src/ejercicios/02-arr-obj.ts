46d99e0558096b9c02b9450999c9279895df421e
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-01-27 21:32:40 +0100

    FIX

--- TerceraSeccion/curso-angular-intro-typescript-ecmascript-1.0.1/src/ejercicios/02-arr-obj.ts
+++ TerceraSeccion/curso-angular-intro-typescript-ecmascript-1.0.1/src/ejercicios/02-arr-obj.ts
@@ -1,1 +1,25 @@
 
+/* 
+//let habilidades: (string | boolean | number)[] = ['bash','healing','counter'];
+habilidades.push('dps')
+// mi array solo deve contener un solo dato
+
+
+//objeto literal
+//no usar tipo any
+//las interfaces no existen en javascrip
+
+interface Personaje {
+   nombre: string;
+   hp: number;
+   habilidades : string[];
+   puebloNatal?:string; // el ?: me ayuda a decir que una propiedad puede ser opcional y no deve ser declarada necesariamente
+}
+const personaje : Personaje = {
+ nombre: 'ale',
+ hp:100,
+ habilidades:['Bash']
+
+}
+
+personaje.puebloNatal = 'pueblo paleta' */
