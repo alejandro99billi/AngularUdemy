e870a421d90e621709c1bd2ccf742b84678ec951
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-01-30 19:19:09 +0100

    add pipe |

--- AngularSeccion/PrimerProyectoAngular/bases/src/app/dbz/main-page/main-page.component.ts
+++ AngularSeccion/PrimerProyectoAngular/bases/src/app/dbz/main-page/main-page.component.ts
@@ -1,12 +1,40 @@
 import { Component, OnInit } from '@angular/core';
 
+
+  
+interface Personaje{
+  nombre: string,
+  poder: number
+}
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrls: ['./main-page.component.scss']
 })
 export class MainPageComponent implements OnInit {
+nuevo: Personaje ={
+  nombre: '',
+  poder: 0
+}
 
+personajes: Personaje[]=[
+  {nombre:'goku',poder:20000},
+  {nombre:'vegeta',poder:18500}
+]
+
+  agregar(){
+// event.preventDefault();
+   if(this.nuevo.nombre.trim().length === 0){
+  return
+   }
+   this.personajes.push(this.nuevo)
+   this.nuevo={
+    nombre:'',
+    poder:0
+   }
+  }
+
+ 
   constructor() { }
 
   ngOnInit(): void {
