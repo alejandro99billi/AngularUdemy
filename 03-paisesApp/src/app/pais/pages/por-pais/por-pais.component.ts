eb7c75f9ac4603985f0ec04018a0bf5decb10e66
alejandro99billi <billialejandro.salvatierramer01@universitadipavia.it>
2023-02-04 19:13:59 +0100

    add sevice to por-pais.component

--- 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
+++ 03-paisesApp/src/app/pais/pages/por-pais/por-pais.component.ts
@@ -8,15 +8,21 @@ import { PaisService } from '../../services/pais.service';
 })
 export class PorPaisComponent implements OnInit {
 
-  termino: string = "hola mundo"
+  termino: string = "hola mundo";
+  hayError: boolean = false
   constructor(private paisService : PaisService) { }
 
   ngOnInit(): void {
   }
 
 buscar(){
+  this.hayError = false;
   console.log(this.termino);
-  this.paisService.buscarPais(this.termino).subscribe(resp => console.log(resp));
+  this.paisService.buscarPais(this.termino).subscribe((resp) => {console.log(resp)},
+  (error)=>{
+ this.hayError = true;
+  }
+  );
 }
 
 }
